04cb4fe688e870cc4e65a5d82057e4c00f2c873f
Desmond <desmondmpofu@gmail.com>
2024-05-07 15:32:55 +0200

    geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from './loc.js'
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [placesByDistance, setPlacesByDistance]= useState([])
+
+  useEffect(()=>{
+      navigator.geolocation.getCurrentPosition((position)=>{
+        const sortedPlaces= sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude)
+        setPlacesByDistance(sortedPlaces)
+      } )
+      
+  }, [])
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -63,7 +73,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          fallbackText='Sorting places...'
+          places={placesByDistance}
           onSelectPlace={handleSelectPlace}
         />
       </main>
